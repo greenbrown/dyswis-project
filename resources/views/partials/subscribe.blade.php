7b406e79b6194665f210c7bbcceb7ce5f6cc9ae5
stx21 <greenbrown003@gmail.com>
2022-11-23 22:10:04 +0700

    Revert "install bootstrap-icons"
    This reverts commit 44a99e6f36a322209ebc880f38c72688e5f6a8d1.

--- resources/views/partials/subscribe.blade.php
+++ resources/views/partials/subscribe.blade.php
@@ -1,12 +1,19 @@
 <div class="site-blocks-cover overlay inner-page-cover" style="background-image: url(images/hero_bg_1.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
-  <div class="container">
-     <div class="row align-items-center justify-content-center text-center">
-      <div class="col-md-6" data-aos="fade-up" data-aos-delay="100">
-        <h2>Available on</h2>
-        <p class="mb-5">Listen and follow us in your favorite platform.</p>
-        <a class="btn btn-primary" style="background-color: #ed302f;" href="#!" role="button"><i class="fab fa-youtube"></i></a>
-      </div>
-    </div>
-  </div>
-</div> 
+    <div class="container">
+        <div class="row align-items-center justify-content-center text-center">
 
+          <div class="col-md-6" data-aos="fade-up" data-aos-delay="100">
+            <h2>Subscribe</h2>
+            <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit nihil saepe libero sit odio obcaecati veniam.</p>
+            <form action="#" method="post" class="site-block-subscribe">
+              <div class="input-group mb-3">
+                <input type="text" class="form-control border-secondary text-white bg-transparent" placeholder="Enter Email" aria-label="Enter Email" aria-describedby="button-addon2">
+                <div class="input-group-append">
+                  <button class="btn btn-primary" type="button" id="button-addon2">Send</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+    </div>
+</div>
